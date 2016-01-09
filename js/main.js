312efb314aa34c1936eac36a5e19449eae58a34a
Evelyn Barrios <evy2bar@gmail.com>
2016-01-08 23:46:45 -0500

    final stage of my personal site

--- js/main.js
+++ js/main.js
@@ -6,141 +6,80 @@
 
 function main() {
 
-(function () {
-   'use strict';
+    (function () {
+        'use strict';
 
-    /*====================================
-     Modal
-     ======================================*/
+        /* ==============================================
+         Scroll
+         =============================================== */
 
-    function artistModal() {
-        if (!input) {
-            input = "Nothing Else Matters";
-        }
-
-        $.ajax({
-            url: "https://api.spotify.com/v1/search?type=track&query=" + input,
-
-
-            success: function (response) {
-                var info = response.tracks.items[0].artists[0].href;
-
-                var albumName = response.tracks.items[0].album.name;
-                $(".js-albumName").empty();
-
-                var albumId = response.tracks.items[0].album.id;
-
-                $.ajax({
-                    url: info,
-
-                    success: function (response) {
-                        var name = response.name;
-                        $(".js-name").empty();
-
-                        var popular = response.popularity;
-                        $(".js-popular").empty();
-
-                        var picture = response.images[0];
-
-                        var genres = response.genres;
-                        $(".js-genres").empty();
-                        console.debug(genres);
-
-                        for (var i = 0; i < genres.length; i++) {
-
-                            if (i == genres.length - 1) {
-                                $(".js-genres").append(genres[i] + ".");
-                            }
-                            else {
-                                $(".js-genres").append(genres[i] + ", ");
-                            }
-                        }
-
-                        $('.js-picture').prop('src', picture.url);
-                        $(".js-name").append(name);
-                        $(".js-popular").append(popular);
-                        $(".js-albumName").append(albumName);
-
-                    },
-                    error: function () {
-                        alert("error loadind info")
-                    }
-
-                });
-
-                $(".full-album").prop("href", "https://open.spotify.com/album/" + albumId);
-
-
-            },
-            error: function () {
-                alert("Error loading artist info")
-            }
-
-        });
-    }
-
-
-        $(".author").on("click", function(){
-        $(".js-modal").modal();
-        artistModal();
-    });
-
-    /*====================================
-    Show Menu on Book
-    ======================================*/
-    $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 100;
-        if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
-        } else {
-            $('.navbar-default').removeClass('on');
-        }
-    });
-
-    $('body').scrollspy({ 
-        target: '.navbar-default',
-        offset: 80
-    })
-
-  	$(document).ready(function() {
-
-
-  	});
-
-  	/*====================================
-    Portfolio Isotope Filter
-    ======================================*/
-    $(window).load(function() {
-        var $container = $('#lightbox');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
+        $('a.page-scroll').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 40
+                    }, 900);
+                    return false;
+                }
             }
         });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
+
+        /*====================================
+         Show Menu on Book
+         ======================================*/
+        $(window).bind('scroll', function() {
+            var navHeight = $(window).height() - 100;
+            if ($(window).scrollTop() > navHeight) {
+                $('.navbar-default').addClass('on');
+            } else {
+                $('.navbar-default').removeClass('on');
+            }
+        });
+
+        $('body').scrollspy({
+            target: '.navbar-default',
+            offset: 80
+        })
+
+
+
+        /*====================================
+         Portfolio Isotope Filter
+         ======================================*/
+        $(window).load(function() {
+            var $container = $('#lightbox');
             $container.isotope({
-                filter: selector,
+                filter: '*',
                 animationOptions: {
                     duration: 750,
                     easing: 'linear',
                     queue: false
                 }
             });
-            return false;
+            $('.cat a').click(function() {
+                $('.cat .active').removeClass('active');
+                $(this).addClass('active');
+                var selector = $(this).attr('data-filter');
+                $container.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false
+                    }
+                });
+                return false;
+            });
+
         });
 
-    });
 
 
-
-}());
+    }());
 
 
 }
 main();
+
